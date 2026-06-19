7ea29c152c28e028223a0de52739920b324bb3c8
j-mceach <jemceach@gmail.com>
2026-06-18 21:30:15 -0400

    Update materialization
    - dbt_project.yml
    - dim_hosts_cleansed.sql
    - dim_listings_cleansed.sql

--- airbnb/models/dim/dim_listings_cleansed.sql
+++ airbnb/models/dim/dim_listings_cleansed.sql
@@ -1,3 +1,8 @@
+{{
+    config(
+        materialized = 'view'
+    )
+}}
 WITH src_listings AS (
     SELECT * FROM {{ ref('src_listings')}}
 )
@@ -8,7 +13,7 @@ SELECT
     , room_type
     , CASE WHEN minimum_nights = 0 THEN 1 ELSE minimum_nights END AS minimum_nights 
     , host_id
-    , REPLACE(price_str, '$', '')::NUMBER(10, 2) AS price_str
+    , REPLACE(price_str, '$', '')::NUMBER(10, 2) AS price
     , created_at
     , updated_at 
 FROM src_listings
